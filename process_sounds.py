5c104e365bdbff0476c23f90b70d141c0b730130
Polina Sonina <sarun4ik7@yandex.ru>
2018-03-01 21:59:05 +0300

    upd

--- process_sounds.py
+++ process_sounds.py
@@ -3,15 +3,33 @@ import librosa
 import librosa.display
 import matplotlib.pyplot as plt
 import numpy as np
+from sklearn.ensemble import RandomForestClassifier
 
-def load_sound_files(path):
+def parse_sounds(path):
     raw_sounds = []
+    labels = []
     for filename in os.listdir(path):
-        if filename.startswith("f"):
+        if filename.startswith(""):
             print(filename)
-            X,sr = librosa.load(path+filename)
-            raw_sounds.append(X)
-    return raw_sounds
+            X, sample_rate = librosa.load(path+filename)
+            mfccs,chroma,mel,contrast,tonnetz = extract_feature(X, sample_rate)
+            ext_features = np.hstack([mfccs,chroma,mel,contrast,tonnetz])
+            features = np.vstack([features,ext_features])            
+            
+            label = filename.split(".")[0].split("_")[2]
+            raw_sounds.append(np.array(X, dtype=float))
+            labels.append(label)
+    return raw_sounds, labels
+
+def extract_feature(X, sample_rate):
+    stft = np.abs(librosa.stft(X))
+    mfccs = np.mean(librosa.feature.mfcc(y=X, sr=sample_rate, n_mfcc=40).T,axis=0)
+    chroma = np.mean(librosa.feature.chroma_stft(S=stft, sr=sample_rate).T,axis=0)
+    mel = np.mean(librosa.feature.melspectrogram(X, sr=sample_rate).T,axis=0)
+    contrast = np.mean(librosa.feature.spectral_contrast(S=stft, sr=sample_rate).T,axis=0)
+    tonnetz = np.mean(librosa.feature.tonnetz(y=librosa.effects.harmonic(X),
+    sr=sample_rate).T,axis=0)
+    return mfccs,chroma,mel,contrast,tonnetz
 
 def plot_waves(sound_names,raw_sounds):
     for n,f in zip(sound_names,raw_sounds):
@@ -19,12 +37,20 @@ def plot_waves(sound_names,raw_sounds):
         plt.title(n)
         plt.show()
 
-path = "data/test_mic/"
-sound_names = ["5","6","7","8","9","5","6","7","8","9"]
+path_1 = "data/test_mic/"
+path_2 = "data/test_phone/"
 
-raw_sounds = load_sound_files(path)
+train_features, train_labels = parse_sounds(path_1)
+test_features, test_labels = parse_sounds(path_2)
 
-plot_waves(sound_names,raw_sounds)
+clf = RandomForestClassifier(n_jobs=2, random_state=0)
+
+clf.fit(raw_sounds, labels)
+
+print(clf.feature_importances_)
+
+print(clf.predict(test_sounds))
+print(test_labels)
 
 #y = raw_sounds[5]
 
