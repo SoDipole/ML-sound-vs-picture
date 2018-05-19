30eadc881766fd5e05df349d2900b43c7d5c6dfb
Polina Sonina <sarun4ik7@yandex.ru>
2018-05-19 19:55:03 +0300

    upd

---
+++ prepare_data.py
@@ -0,0 +1,31 @@
+import os
+import librosa
+import numpy as np
+
+path = "data/raw/"
+out_path = "data/ready/"
+
+if not os.path.exists(out_path):
+    os.makedirs(out_path)
+
+maxv = np.iinfo(np.int16).max
+
+person_id = 12
+
+for filename in os.listdir(path):
+    if filename.startswith("Запись_0006"):
+        person_id += 1     
+        print(filename)
+        X, sample_rate = librosa.load(path+filename)
+        
+        chunks = librosa.effects.split(X, top_db=30, frame_length=5500)
+        
+        print(len(chunks))
+        
+        label = 0
+        for chunk in chunks:
+            label += 1
+            y = X[chunk[0]:chunk[1]]
+            librosa.output.write_wav(out_path+str(person_id)+"_"+str(label)+".wav", 
+                                     (y*maxv).astype(np.int16),
+                                     sample_rate)
--- process_sounds.py
+++ process_sounds.py
@@ -3,28 +3,33 @@ import librosa
 import librosa.display
 import matplotlib.pyplot as plt
 import numpy as np
-from sklearn.gaussian_process import GaussianProcessClassifier
 from sklearn.neighbors import KNeighborsClassifier
 
 def parse_sounds(path):
-    features, labels = np.empty((0,193)), np.empty(0)
+    #features, labels = np.empty((0,193)), np.empty(0)
+    features, labels = np.empty((0,187)), np.empty(0)
     for filename in os.listdir(path):
         if filename.startswith(""):
             print(filename)
             X, sample_rate = librosa.load(path+filename)
-            plot_waves(X)            
-
-            X, index = librosa.effects.trim(X, top_db=20)
-            print(index)
-            plot_waves(X)
             
-            mfccs,chroma,mel,contrast,tonnetz = extract_feature(X, sample_rate)
+            #plot_waves(X)            
             
-            ext_features = np.hstack([mfccs,chroma,mel,contrast,tonnetz])
+            #S = librosa.feature.melspectrogram(y=X, sr=sample_rate)
+            #librosa.display.specshow(librosa.power_to_db(S, ref=np.max), 
+                                     #y_axis='mel', x_axis='time')
+            #plt.title('Mel-scaled рpower spectrogram')
+            #plt.show()            
+            
+            mfccs,chroma,mel,contrast = extract_feature(X, sample_rate)
+            
+            ext_features = np.hstack([mfccs,chroma,mel,contrast])
+            #print(len(ext_features))
             features = np.vstack([features,ext_features])            
             
-            label = filename.split(".")[0].split("_")[2]
+            label = filename.split(".")[0].split("_")[1]
             labels = np.append(labels, label)
+            
     return np.array(features), np.array(labels, dtype = np.int)
 
 def extract_feature(X, sample_rate):
@@ -37,36 +42,23 @@ def extract_feature(X, sample_rate):
                   axis=0)
     contrast = np.mean(librosa.feature.spectral_contrast(S=stft, sr=sample_rate).T,
                        axis=0)
-    tonnetz = np.mean(librosa.feature.tonnetz(y=librosa.effects.harmonic(X), 
-                                              sr=sample_rate).T,axis=0)
-    return mfccs,chroma,mel,contrast,tonnetz
+    return mfccs,chroma,mel,contrast
 
 def plot_waves(raw_sound):
     librosa.display.waveplot(raw_sound)
     plt.show()
 
-path_1 = "data/test_mic/train/"
-path_2 = "data/test_mic/test/"
-#path_2 = "data/test_phone/"
+path_1 = "data/ready/train/"
+path_2 = "data/ready/test/"
+
+#train_features, train_labels = parse_sounds("data/ready/")
 
 train_features, train_labels = parse_sounds(path_1)
 test_features, test_labels = parse_sounds(path_2)
 
-#clf = GaussianProcessClassifier()
 clf = KNeighborsClassifier(n_neighbors=1)
 
 clf.fit(train_features, train_labels)
 
 print(clf.predict(test_features))
 print(test_labels)
-
-#y = raw_sounds[5]
-
-#plt.plot(raw_sounds[5], label='пять')
-#plt.show()
-
-#D = librosa.amplitude_to_db(librosa.stft(y), ref=np.max)
-#librosa.display.specshow(D, y_axis='linear')
-#plt.colorbar(format='%+2.0f dB')
-#plt.title('Linear-frequency power spectrogram')
-#plt.show()
