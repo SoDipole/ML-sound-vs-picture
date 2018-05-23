67be401839003e5508074429547306b41999a6ab
Polina Sonina <sarun4ik7@yandex.ru>
2018-05-24 02:12:44 +0300

    upd

--- process_images.py
+++
@@ -1,9 +0,0 @@
-import os
-from scipy import ndimage
-import matplotlib.pyplot as plt
-from scipy import misc
-
-spectrogram = misc.imread("data_image/f1_mic_5_4kHz.png")
-
-plt.imshow(spectrogram)
-plt.show()
